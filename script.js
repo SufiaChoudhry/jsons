d1dff38135b27c28cc7cdfddc34730e0396c86b3
SufiaChoudhry <49155770+SufiaChoudhry@users.noreply.github.com>
2019-07-13 12:40:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+let anobject = {
+    "firstName": "John",
+    "lastName": "Smith",
+    "address": {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": 10021
+    },
+    "phoneNumbers": [
+        "212 555-1234",
+        "646 555-4567"
+    ]
+}
+
+let kfcresturant = {
+    everyDayValue: {
+        riceAndSpice: "PKR 250"[
+            rice,
+            spices
+        ],
+
+        krunchBurger: "PKR 250"[
+            oneTwister,
+            oneFries,
+            drink,
+            colslaw,
+            onePcsChicken
+        ],
+
+        krunchBurgerWithDrink: "PKR 250"[
+            fourZingerStrips,
+            oneColeslaw,
+            fries,
+            drink,
+            oneDip,
+            onedinnerRoll
+        ],
+        
+        krunchCombo: "PKR 250",
+        chickenAndChips: "PKR 250",
+        chickenndRice: "PKR 250"
+    },
+
+    makeItAMeal: {
+        kentucky: "PKR 440",
+        zingerStacker: "PKR 440",
+        zinger: "PKR 440",
+        mightyZinger: "PKR 440",
+        twister: "PKR 440"
+    },
+    mealBox: {
+        twistyBox: "PKR 580",
+        bonelessBox: "PKR 580",
+        wOWBox: "PKR 580",
+        xtremeBox: "PKR 580",
+        crispyBox: "PKR 580"
+
+    },
+    Sharing: {
+        valueBucket: "PKR 1,150",
+        familyBucket: "PKR 1,150",
+        familyFestivalOne: "PKR 1,150",
+        familyFestivalTwo: "PKR 1,150"
+    },
+    Promotions: {
+        zingerTrio: "PKR 1,300",
+        lunchDeal: "PKR 1,500",
+        megaJackpotDeal: "PKR 1,600",
+        mingleBucket: "PKR 1,700",
+        xtremeBox: "PKR 1,800",
+        crispyBox: "PKR 2000"
+    },
+    Snacks: {
+        chickyMealOne: "PKR",
+        coleslaw: "PKR",
+        snackBucket: "PKR",
+        frenchFries: "PKR",
+        frenchFriesBucket: "PKR",
+        sixPcsNuggets: "PKR",
+        ninePcsNuggets: "PKR",
+        hotShotsNinePcs: "PKR",
+        hotWingsTenPcs: "PKR",
+        arabianRice: "PKR",
+        cornOnTheCobRegular: "PKR",
+        pepsi: "PKR",
+        sevevup: "PKR",
+        marinda: "PKR",
+        mineralWater: "PKR",
+        mountainDew: "PKR",
+        dietPepsi: "PKR"
+    },
+
+    midnightDeals: {
+        midnightDealOne: "PKR",
+        midnigtDealTwo: "PKR",
+        midnightDealThree: "PKR",
+        midnightDealfour: "PKR"
+    }
+}
+
